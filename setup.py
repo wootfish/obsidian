cf8cbb02fa6f6a5b995dcfb75fd2c4001016f7ef
Eli Sohl <elisohl@gmail.com>
2020-03-30 02:14:01 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='obsidian',
+    version='0.0.0',
+    description='Constraint-based visual design',
+    packages=['obsidian'],
+    install_requires=['pysmt'],
+)

26abf0f9f92e4207e29d28410ba42b378fe94e68
Eli Sohl <elisohl@gmail.com>
2020-03-31 16:10:04 -0700

    square_grid.py now uses shape_grid()

--- examples/square_grid.py
+++ examples/square_grid.py
@@ -1,61 +1,33 @@
 from random import Random
+choice = Random(3**20 % (2**20-1)).choice  # arbitrary nontrivial constant seed
 
 from obsidian.align import top_align, left_align
 from obsidian.canvas import Canvas
-from obsidian.shapes import Rectangle
 from obsidian.group import Group
+from obsidian.helpers import shape_grid
 from obsidian.infix import EQ
-
-
-choice = Random(3**20 % (2**20-1)).choice  # arbitrary nontrivial constant seed
+from obsidian.shapes import Rectangle
 
 
 width = 300
 height = 300
 
-
 BG = "#000000"
 RED = "#AA1010"
 BLUE = "#3030AA"
 
-
-def square_grid(w, h, size, spacing):
-    grid = [[Rectangle(width=size, height=size) for _ in range(w)] for _ in range(h)]
-    constraints = []
-
-    # align rows and columns
-    for row in grid:
-        constraints += top_align(row)
-    for col in range(w):
-        constraints += left_align(row[col] for row in grid)
-
-    # establish spacing between cells
-    first_row = grid[0]
-    first_col = [row[0] for row in grid]
-
-    for a, b in zip(first_row, first_row[1:]):
-        constraints.append(b.x |EQ| a.x + size + spacing)
-
-    for a, b in zip(first_col, first_col[1:]):
-        constraints.append(b.y |EQ| a.y + size + spacing)
-
-    shapes = [rect for row in grid for rect in row]  # flattens the grid
-    return Group(shapes, constraints)
-
-
 bg = Rectangle(0, 0, width, height, {"fill": BG})
 
+squares = shape_grid(w=20, h=20, spacing=3,
+        factory=lambda: Rectangle(width=10, height=10))
 
-squares = square_grid(w=20, h=20, size=10, spacing=3)
 for square in squares.shapes:
     square.style = {"fill": choice((RED, BLUE))}
-
 
 g = Group([squares, bg], [
     squares.bounds.center.x |EQ| bg.bounds.center.x,
     squares.bounds.center.y |EQ| bg.bounds.center.y,
     ])
-
 
 print("Working...")
 canvas = Canvas(g, width, height)

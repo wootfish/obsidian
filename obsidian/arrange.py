b80e43219abc91423e45b839c76445bf3ca42a54
Eli Sohl <elisohl@gmail.com>
2020-04-01 00:38:01 -0700

    added evenly_spaced()

--- obsidian/arrange.py
+++ obsidian/arrange.py
@@ -20,3 +20,16 @@ def top_align(shapes):
 def bottom_align(shapes):
     s = FreshSymbol(REAL)
     return [Equals(shape.bounds.bottom_edge, s) for shape in shapes]
+
+
+def evenly_spaced(start_point, end_point, shapes):
+    n = len(shapes)
+    assert n > 1
+    constraints = []
+    for i in range(n):
+        j = n - i - 1
+        x = (start_point.x*j + end_point.x*i) / (n-1)
+        y = (start_point.y*j + end_point.y*i) / (n-1)
+        constraints.append(Equals(shapes[i].x, x))
+        constraints.append(Equals(shapes[i].y, y))
+    return constraints

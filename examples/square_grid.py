c38c594df534c5978cfc16fb06fd69bfec6b437d
Eli Sohl <elisohl@gmail.com>
2020-04-08 17:20:06 -0700

    made square_grid.py simpler & more interesting

--- examples/square_grid.py
+++ examples/square_grid.py
@@ -1,7 +1,3 @@
-from random import Random
-choice = Random(3**20 % (2**20-1)).choice  # arbitrary nontrivial constant seed
-
-from obsidian.arrange import top_align, left_align
 from obsidian.canvas import Canvas
 from obsidian.group import Group
 from obsidian.helpers import shape_grid
@@ -9,27 +5,21 @@ from obsidian.infix import EQ
 from obsidian.shapes import Rectangle
 
 
-width = 300
-height = 300
+grid_w = 5
+grid_h = 2**5
 
 BG = "#000000"
 RED = "#AA1010"
 BLUE = "#3030AA"
 
-bg = Rectangle(0, 0, width, height, {"fill": BG})
-
-squares = shape_grid(w=20, h=20, spacing=3,
+squares = shape_grid(w=grid_w, h=grid_h, spacing=3,
         factory=lambda: Rectangle(width=10, height=10))
 
-for square in squares.shapes:
-    square.style = {"fill": choice((RED, BLUE))}
-
-g = Group([squares, bg], [
-    squares.bounds.center.x |EQ| bg.bounds.center.x,
-    squares.bounds.center.y |EQ| bg.bounds.center.y,
-    ])
+for i, square in enumerate(squares.shapes):
+    color = RED if (i // grid_w) & (2**(i%grid_w)) else BLUE
+    square.style = {"fill": color}
 
 print("Working...")
-canvas = Canvas(g, width, height)
+canvas = Canvas(squares, bg_color=BG)
 canvas.save_png("gallery/square_grid.png")
 canvas.save_svg("gallery/square_grid.svg")

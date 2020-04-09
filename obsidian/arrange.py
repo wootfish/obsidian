7ec6e2b88ea14604f104f48be4bab609a74a3bc8
Eli Sohl <elisohl@gmail.com>
2020-04-09 16:10:21 -0700

    made obsidian.arrange methods return single constraints

--- obsidian/arrange.py
+++ obsidian/arrange.py
@@ -1,25 +1,25 @@
-from pysmt.shortcuts import FreshSymbol, Equals
+from pysmt.shortcuts import FreshSymbol, Equals, And
 from pysmt.typing import REAL
 
 
 def left_align(shapes):
     s = FreshSymbol(REAL)
-    return [Equals(shape.bounds.left_edge, s) for shape in shapes]
+    return And(Equals(shape.bounds.left_edge, s) for shape in shapes)
 
 
 def right_align(shapes):
     s = FreshSymbol(REAL)
-    return [Equals(shape.bounds.right_edge, s) for shape in shapes]
+    return And(Equals(shape.bounds.right_edge, s) for shape in shapes)
 
 
 def top_align(shapes):
     s = FreshSymbol(REAL)
-    return [Equals(shape.bounds.top_edge, s) for shape in shapes]
+    return And(Equals(shape.bounds.top_edge, s) for shape in shapes)
 
 
 def bottom_align(shapes):
     s = FreshSymbol(REAL)
-    return [Equals(shape.bounds.bottom_edge, s) for shape in shapes]
+    return And(Equals(shape.bounds.bottom_edge, s) for shape in shapes)
 
 
 def evenly_spaced(start_point, end_point, shapes):
@@ -32,4 +32,4 @@ def evenly_spaced(start_point, end_point, shapes):
         y = (start_point.y*j + end_point.y*i) / (n-1)
         constraints.append(Equals(shapes[i].x, x))
         constraints.append(Equals(shapes[i].y, y))
-    return constraints
+    return And(constraints)

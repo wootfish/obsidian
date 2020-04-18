b8cceb874e787bd3c8f13ffaf12fc739dc80f910
Eli Sohl <elisohl@gmail.com>
2020-04-18 14:01:48 -0700

    added center_align_{x,y} arrangement helpers

--- obsidian/arrange.py
+++ obsidian/arrange.py
@@ -2,6 +2,10 @@ from pysmt.shortcuts import FreshSymbol, Equals, And
 from pysmt.typing import REAL
 
 
+# TODO these should probably take varargs, so we can just do e.g.
+# left_align(shape_1, shape_2, shape_3)
+
+
 def left_align(shapes):
     s = FreshSymbol(REAL)
     return And(Equals(shape.bounds.left_edge, s) for shape in shapes)
@@ -20,6 +24,22 @@ def top_align(shapes):
 def bottom_align(shapes):
     s = FreshSymbol(REAL)
     return And(Equals(shape.bounds.bottom_edge, s) for shape in shapes)
+
+
+def center_align_x(shapes):
+    """
+    Returns a constraint setting the shapes' centers' x-coordinates equal.
+    """
+    s = FreshSymbol(REAL)
+    return And(Equals(shape.center.x, s) for shape in shapes)
+
+
+def center_align_y(shapes):
+    """
+    Returns a constraint setting the shapes' centers' y-coordinates equal.
+    """
+    s = FreshSymbol(REAL)
+    return And(Equals(shape.center.y, s) for shape in shapes)
 
 
 def evenly_spaced(start_point, end_point, shapes):

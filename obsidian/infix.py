1b1056744e13321727ffc026f1ac625ab54b38f8
Eli Sohl <elisohl@gmail.com>
2020-04-18 13:47:34 -0700

    added infix operators for relative positioning

--- obsidian/infix.py
+++ obsidian/infix.py
@@ -1,15 +1,10 @@
 """
-Notes:
-may want to expand the list of supported comparisons
+Note: may want to expand the list of supported comparisons
 (to include eg GT LT GE LE)
-
-also, "not" is currently omitted - should we export pysmt.shortcuts.Not as NOT
-for consistency? even though it's not an infix? if we're doing that, should we
-rename this file?
 """
 
 
-__all__ = ['EQ', 'NE']
+__all__ = ['EQ', 'NE', 'ABOVE_BY', 'BELOW_BY', 'LEFT_BY', 'RIGHT_BY']
 
 
 from functools import wraps
@@ -72,3 +67,55 @@ def point_not_equals(lhs, rhs):
 
 EQ = Infix(real_wrapper(point_equals))
 NE = Infix(real_wrapper(point_not_equals))
+
+
+def ABOVE_BY(margin):
+    """
+    Returns an infix operator that takes two Shape arguments and returns a
+    constraint positioning the first Shape's lower edge above the second Shape's
+    top edge with a distance defined by `margin`.
+
+    For instance, to position shape_1 ten pixels above shape_2, use:
+    >>> shape_1 | ABOVE_BY(10) | shape_2
+
+    The above expression is equivalent to:
+    >>> shape_1.bounds.bottom_edge + 10 |EQ| shape_2.bounds.top_edge
+    """
+
+    return Infix(lambda s1, s2: Equals(
+        s1.bounds.bottom_edge + margin,
+        s2.bounds.top_edge
+    ))
+
+
+def BELOW_BY(margin):
+    """
+    Like ABOVE_BY but places the first shape below the second one.
+    """
+
+    return Infix(lambda s1, s2: Equals(
+        s1.bounds.top_edge - margin,
+        s2.bounds.bottom_edge
+    ))
+
+
+def LEFT_BY(margin):
+    """
+    Like ABOVE_BY but places the first shape to the left of the second one.
+    """
+
+    return Infix(lambda s1, s2: Equals(
+        s1.bounds.right_edge + margin,
+        s2.bounds.left_edge
+    ))
+
+
+def RIGHT_BY(margin):
+    """
+    Like ABOVE_BY but places the first shape to the right of the second one.
+    """
+
+    return Infix(lambda s1, s2: Equals(
+        s1.bounds.left_edge - margin,
+        s2.bounds.right_edge
+    ))
